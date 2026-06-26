d2c73d639f3c6f8cb24a6d1ee8102ba70bd9f076
Krish Shaw <shawkrish418@gmail.com>
2026-06-26 18:14:14 +0530

    feat: add withdraw & close instructions

--- programs/lamports-vault/src/instructions/close.rs
+++ programs/lamports-vault/src/instructions/close.rs
@@ -5,10 +5,36 @@ use crate::{VAULT_SEED, VAULT_STATE_SEED, VaultState};
 #[derive(Accounts)]
 pub struct Close<'info> {
     #[account(mut)]
-    pub user: Signer<'info>,
+    pub user: Signer<'info>, // To pay for instruction call
+    #[account(mut, seeds=[VAULT_SEED, user.key().as_ref()], bump = vault_state.vault_bump)]
+    pub vault: SystemAccount<'info>, // To pull deposited lamports and rent
+    #[account(mut, close = user, seeds = [VAULT_STATE_SEED, user.key().as_ref()], bump = vault_state.bump)]
+    pub vault_state: Account<'info, VaultState>, // To close vault_state data (i.e. bumps)
+    pub system_account: Program<'info, System> // To transfer vault lamports to user
 }
 
 pub fn close_vault(ctx: Context<Close>) -> Result<()> {
+    msg!("Closing vault & withdrawing lamports...");
+    let lamports = ctx.accounts.vault.lamports();
+
+    let seeds = &[
+        VAULT_SEED,
+        ctx.accounts.user.key.as_ref(),
+        &[ctx.accounts.vault_state.vault_bump],
+    ];
+
+    let signer = &[&seeds[..]];
+
+
+    let cpi_accounts = anchor_lang::system_program::Transfer {
+        from: ctx.accounts.vault.to_account_info(),
+        to: ctx.accounts.user.to_account_info()
+    };
+
+    let cpi_ctx = CpiContext::new_with_signer(ctx.accounts.system_account.key(), cpi_accounts, signer);
+
+    anchor_lang::system_program::transfer(cpi_ctx, lamports)?;
+    msg!("Vault closed! Amount withdrawn: {}", lamports);
 
     Ok(())
 }
--- programs/lamports-vault/src/instructions/withdraw.rs
+++ programs/lamports-vault/src/instructions/withdraw.rs
@@ -5,18 +5,34 @@ use crate::{VAULT_SEED, VAULT_STATE_SEED, VaultState};
 #[derive(Accounts)]
 pub struct Withdraw<'info> {
     #[account(mut)]
-    pub user: Signer<'info>,
-
+    pub user: Signer<'info>, // The user signing/paying for instruction call
+    #[account(mut, seeds=[VAULT_SEED, user.key().as_ref()], bump = vault_state.vault_bump)]
+    pub vault: SystemAccount<'info>, // The vault account that holds the SOL of the user
+    #[account(seeds=[VAULT_STATE_SEED, user.key().as_ref()], bump = vault_state.bump)]
+    pub vault_state: Account<'info, VaultState>, // To get the bump for vault PDA derivation
+    pub system_program: Program<'info, System> // To transfer sol to owner of vault
 }
 
 pub fn withdraw_lamports(ctx: Context<Withdraw>, amount: u64) -> Result<()> {
+    msg!("Withdrawing lamports to vault");
 
-    let signer_seeds = &[
+    let seeds = &[
         VAULT_SEED,
-        &ctx.accounts.user.key().to_bytes(),
-        &[ctx.accounts.vault_state.vault_bump],
+        ctx.accounts.user.key.as_ref(),
+        &[ctx.accounts.vault_state.vault_bump]
     ];
-    let binding = [&signer_seeds[..]];
+
+    let signer = &[&seeds[..]];
+
+    let cpi_accounts = anchor_lang::system_program::Transfer {
+        from: ctx.accounts.vault.to_account_info(),
+        to: ctx.accounts.user.to_account_info()
+    };
+
+    let cpi_ctx = CpiContext::new_with_signer(ctx.accounts.system_program.key(), cpi_accounts, signer);
+
+    anchor_lang::system_program::transfer(cpi_ctx, amount)?;
+    msg!("Withdrawn {} lamports from vault!", amount);
 
     Ok(())
 }
